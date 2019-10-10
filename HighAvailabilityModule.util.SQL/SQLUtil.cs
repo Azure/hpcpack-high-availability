2bcc46549d7f45d00618a6181ab8287bb1cf2ed2
Xiang Li <t-xial@microsoft.com>
2019-10-10 13:50:58 +0800

    Modify SQLUtil.

--- HighAvailabilityModule.util.SQL/SQLUtil.cs
+++ HighAvailabilityModule.util.SQL/SQLUtil.cs
@@ -3,13 +3,20 @@
 namespace Microsoft.Hpc.HighAvailabilityModule.Util.SQL
 {
     using System;
+    using System.Linq;
 
     public class SQLUtil
     {
         public string GetConStr(string conStr, TimeSpan operationTimeout)
         {
-            return (conStr.IndexOf("Connect Timeout") == -1 ? conStr : conStr.Substring(0, conStr.IndexOf("Connect Timeout")))
-                + "Connect Timeout=" + Convert.ToInt32(Math.Ceiling(operationTimeout.TotalSeconds)).ToString();
+            if (operationTimeout == default(TimeSpan))
+            {
+                return conStr;
+            }
+            else
+            {
+                return string.Join(";", conStr.Split(';').Where(s => !s.Contains("Connect Timeout")).Concat(new[] { "Connect Timeout=" + Convert.ToInt32(Math.Ceiling(operationTimeout.TotalSeconds)).ToString() }));
+            }
         }
     }
 }
